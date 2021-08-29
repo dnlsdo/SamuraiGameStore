f23b4c489d1caf8d959656ca557f27afc6934fca
Danilo Souza <danilo.suza203@hotmail.com>
2021-08-29 02:45:42 -0300

    Barra de pesquisa incluida

---
+++ script.js
@@ -0,0 +1,22 @@
+const nav = document.querySelector(".nav");
+(function adicionaNav(){
+nav.innerHTML = `
+<input type="checkbox" id="chec">
+<label for="chec">
+    <img src="imagens/controle.png" id="menu">
+</label>
+
+  <nav>
+    <ul>
+      <li><a href="">HOME</a></li>
+      <li><a href="login.html">LOGIN</a></li>
+      <li><a href="">VENDAS</a></li>
+      <li><a href="">CADASTROS</a></li>
+      <li><a href="">RELATÓRIOS</a></li>
+
+    </ul>
+    <img src="imagens/download.png" id="logo">
+  </nav>
+`;
+
+})();
