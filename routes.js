d78b07daee9e794180fe728a261fb28dd7cf91a4
G4brIel-Sh4rp <gabriel.pds2001@gmail.com>
2021-10-03 13:48:58 -0300

    Criado viwe relatorioCliente

--- routes.js
+++ routes.js
@@ -5,19 +5,24 @@ const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
 const userController = require('./src/controllers/userController');
 const cadastroController = require('./src/controllers/cadastroContoller');
+const relatorioController = require('./src/controllers/relatorioController');
 
 router.get('/', homeController.index);
+//Login
 router.get('/login', loginController.index);
 router.post('/login/login', loginController.login);
+//Tela User
 router.get('/user', middleware.loginRequired, userController.index);
 router.post('/user/alter', middleware.loginRequired, loginController.alter);
+//Cadastros
 router.get('/cadastro/funcionario', cadastroController.funcionario);
 router.post('/cadastro/funcionario', middleware.loginRequired, loginController.create);
 router.get('/cadastro/cliente',  middleware.loginRequired, cadastroController.cliente);
 router.post('/cadastro/cliente', middleware.loginRequired, cadastroController.createCliente);
 router.get('/cadastro/produto', cadastroController.produto);
 router.post('/cadastro/produto', cadastroController.createProduto);
-
+//Relatórios
+router.get('/relatorio/cliente', relatorioController.showClientes);
 
 router.get('/404', (req, res)=>{
     res.render('404')
