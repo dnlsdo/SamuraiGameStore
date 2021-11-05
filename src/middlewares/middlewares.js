ea2fa483e11137bca7764aba997ed0afe674c9f8
G4brIel-Sh4rp <gabriel.pds2001@gmail.com>
2021-11-05 00:37:44 -0300

    Feature: AdminRequired middleware

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -3,11 +3,23 @@ exports.middlewareGlobal = (req, res, next) => {
     res.locals.erros = req.flash('erros');
     res.locals.success = req.flash('success');
     res.locals.user = req.session.user;
+    if(res.locals.user){
+      res.locals.acesso = res.locals.user.acesso;
+    }else{
+      res.locals.acesso = 2;
+    }
     next();
   };
 
 exports.loginRequired= (req, res, next) =>{
   if(!req.session.user) {
+    res.status(403).render('403');
+    return
+  }
+  next();
+}
+exports.adminRequired = (req, res, next)=>{
+  if(!req.session.user || !req.session.acesso !== 0){
     res.status(403).render('403');
     return
   }
